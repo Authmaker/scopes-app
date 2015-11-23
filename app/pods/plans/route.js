a0d593404227a836c2a80137afaec2fa25f40b8e
Darragh O'Keeffe <hey@darraghokeeffe.com>
2015-11-23 13:18:18 +0000

    added query param for scope on plan route

--- app/pods/plans/route.js
+++ app/pods/plans/route.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: {
+    scope: {
+      refreshModel: true
+    },
+  },
+  model: function(params) {
+    console.log(params);
+    return this.store.find('plans', params);
+  },
 });
