c805ecfd0f167b3d40a4000bae4822a9f656df7e
Darragh O'Keeffe <hey@darraghokeeffe.com>
2015-11-24 20:19:42 +0000

    pull in card data for customer on payment page

--- app/pods/card/model.js
+++ app/pods/card/model.js
@@ -1,12 +1,17 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 var attr = DS.attr;
 
 export default DS.Model.extend({
+  default: attr('boolean'),
   name: attr('string'),
   brand: attr('string'),
   country: attr('string'),
   last4: attr('string'),
   exp_month: attr('string'),
-  exp_year: attr('string')
+  exp_year: attr('string'),
+  logo: Ember.computed('brand', function(){
+    return this.get('brand').toLowerCase();
+  })
 });
--- app/pods/plans/payment/controller.js
+++ app/pods/plans/payment/controller.js
@@ -2,8 +2,16 @@ import Ember from 'ember';
 // import Config from 'authmaker-scopes-app/config/environment';
 
 export default Ember.Controller.extend({
+  payButtonDisabled: true,
 
   actions: {
+    cardChanged: function(card){
+      if(card === 'newCard') {
+        return this.set('payButtonDisabled', false);
+      }
+      return this.set('payButtonDisabled', true);
+    },
+
     processStripeToken: function(token){
       return Ember.$.ajax({
         method: 'POST',
